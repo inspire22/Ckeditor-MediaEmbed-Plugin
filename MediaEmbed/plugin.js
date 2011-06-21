23a45a3d36752e88072e18298ed2f3dce93070fe
Kevin Watt <kevin@allpoetry.com>
2011-06-20 19:15:37 -0700

    initial import of updated code

---
+++ MediaEmbed/plugin.js
@@ -0,0 +1,63 @@
+/*
+* @example An iframe-based dialog with custom button handling logics.
+*/
+( function() {
+    CKEDITOR.plugins.add( 'MediaEmbed',
+    {
+        requires: [ 'iframedialog' ],
+        init: function( editor )
+        {
+           var me = this;
+           CKEDITOR.dialog.add( 'MediaEmbedDialog', function (editor)
+           {
+              return {
+                 title : 'Embed Media Dialog',
+                 minWidth : 550,
+                 minHeight : 200,
+                 contents :
+                       [
+                          {
+                             id : 'iframe',
+                             label : 'Embed Media',
+                             expand : true,
+                             elements :
+                                   [
+                                      {
+                           type : 'html',
+                           id : 'pageMediaEmbed',
+                           label : 'Embed Media',
+                           style : 'width : 100%;',
+                           html : '<iframe src="'+me.path+'/dialogs/mediaembed.html" frameborder="0" name="iframeMediaEmbed" id="iframeMediaEmbed" allowtransparency="1" style="width:100%;margin:0;padding:0;"></iframe>'
+                          }
+                                   ]
+                          }
+                       ],
+                 // this gets called when Ok is prssed.
+                 // stephen: Hey, I'd append ?wmode=transparent to all links... ie for Ie https://www.youtube.com/embed/kRvL6K8SEgY?wmode=transparent Otherwise the youtube/vimeo will kill overlays - not a problem for me yet, but anyone who wants to submit changes I'd edit it.
+                 onOk : function()
+                 {
+                  for (var i=0; i<window.frames.length; i++) {
+                     if(window.frames[i].name == 'iframeMediaEmbed') {
+                        var content = window.frames[i].document.getElementById("embed").value;
+                     }
+                  }
+                  final_html = 'MediaEmbedInsertData|---' + escape('<span class="media_embed">'+content+'</span>') + '---|MediaEmbedInsertData';
+                    editor.insertHtml(final_html);
+                    updated_editor_data = editor.getData();
+                    clean_editor_data = updated_editor_data.replace(final_html,'<span class="media_embed">'+content+'</span>');
+                    editor.setData(clean_editor_data);
+                 }
+              };
+           } );
+
+          editor.addCommand( 'MediaEmbed', new CKEDITOR.dialogCommand( 'MediaEmbedDialog' ) );
+
+          editor.ui.addButton( 'MediaEmbed',
+          {
+              label: 'Embed Media',
+              command: 'MediaEmbed',
+              icon: this.path + 'images/icon.gif'
+          } );
+        }
+    } );
+} )();
---
+++ readme.txt
@@ -0,0 +1,14 @@
+Installing the MediaEmbed Plugin
+
+1. Copy the "MediaEmbed" folder and place it in the ~/ckeditor/plugins directory.
+
+2. Enable the plugin by changing or adding the extraPlugins line in your configuration (config.js):
+
+    config.extraPlugins = 'MediaEmbed';
+
+3. Add the button to your toolbar by adding the 'MediaEmbed' item to the list.
+
+That's it, overall pretty simple.  This plugin could also be used for inserting custom code snippets, or pretty much anything that requires a custom insert.  Enjoy!
+
+
+Orig by kent@fluidbyte.net.  http://www.fluidbyte.net/embed-youtube-vimeo-etc-into-ckeditor  Edits from comments integrated and posted by Kevin Watt.
